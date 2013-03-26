46c3b19678868a86ca284912f510b762dafaa7c6
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-26 19:04:38 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snippyskippy::Application.config.secret_token = 'ce349b6d4fc2f8df86b9f78ae756c78a771163d6d8ebe943a73cd7ba40dcc8446b435e53327c403871eb1651671e7ea1bfb019e7cf6a04b065a6a6aae9be58dd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Snippyskippy::Application.config.session_store :cookie_store, key: '_snippyskippy_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Snippyskippy::Application.config.session_store :active_record_store
