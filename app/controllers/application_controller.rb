51338a6b31718d8b241e2a67a273b8c0f6b93fa9
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-28 20:02:16 -0400

    Set up sessions and asset pipeline

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+  private
+  def authenticate
+    @auth = (session[:user_id].present?) ? User.find(session[:user_id]) : nil
+  end
 end
