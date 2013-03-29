23da059400fce7cd9a1930ad422543463611b80d
Eric Holland <eric@f-robots.com>
2013-03-29 19:55:21 -0400

    def destroy session testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def login_nav
     nav = ""
     if @auth.present?
-      nav += "<li>#{link_to(@auth.username, '/login', :method => :delete, :class => 'tiny alert')}</li>"
+      nav += "<li>#{link_to(@auth.username + ' | ' + 'Logout', '/login', :method => :delete, :class => 'tiny alert')}</li>"
     else
       nav == ""
     end
