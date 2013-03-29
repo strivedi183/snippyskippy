f934b528ef80cdeea26d14d5228b6c73f951c4ca
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-29 19:33:06 -0400

    Merge issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,6 @@
 //= require 'foundation/js/vendor/custom.modernizr.js'
 //= require 'foundation/js/foundation.min.js'
 //= require 'wookmark.js'
-//= require 'woodmark-tmp.js'
 //= require 'snippy.js'
 //= require 'foundation/js/foundation/foundation.dropdown.js'
 //= require 'foundation/js/foundation/foundation.reveal.js'
