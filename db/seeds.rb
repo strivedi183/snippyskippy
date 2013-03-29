6ee934348a6e6eced72bc13dddcb383454f6a328
Eric Holland <eric@f-robots.com>
2013-03-29 19:28:21 -0400

    add registration to nav

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-Booking.delete_all
 Client.delete_all
 Favorite.delete_all
 Medium.delete_all
@@ -7,3 +6,6 @@ Salon.delete_all
 Stylist.delete_all
 User.delete_all
 
+u1 = User.create(:name => 'Joan Osborne', :email => 'g1ofus@gmail.com', :password => 'a', :password_confirmation => 'a')
+u2 = User.create(:name => 'God', :email => 'god@gmail.com', :password => 'a', :password_confirmation => 'a', :is_admin => true )
+u3 = User.create(:name => 'Snippy', :email => 'snip@gmail.com', :password => 'a', :password_confirmation => 'a')
