f00217f2ebdd5446bf69c7bdef9d6899f81f92c8
Eric Holland <eric@f-robots.com>
2013-03-30 22:09:36 -0400

    add a clientshowpage layoutfile less the nav partial called in the client controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,7 +1,9 @@
 class ClientsController < ApplicationController
+  layout 'clientshowpage'
   before_filter :check_if_logged_in
 
   def show
+    @user = User.new
     @client = Client.find(params[:id])
     @media = Medium.all
     @tags = @media.map(&:tags).flatten.uniq.map(&:tag)
